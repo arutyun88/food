f80323b7ba37f4d9cef299f6b03d0bc830b8ad11
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2023-02-17 00:00:21 +0400

    fonts added

--- lib/app/core/app_theme.dart
+++ lib/app/core/app_theme.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:food/app/core/app_colors.dart';
 
+const String maven = 'Maven';
+
 abstract class AppTheme {
   const AppTheme._();
 
@@ -11,26 +13,31 @@ abstract class AppTheme {
         fontSize: 30.0,
         fontWeight: FontWeight.w500,
         color: AppColors.text,
+        fontFamily: maven,
       ),
       headlineSmall: TextStyle(
         fontSize: 24.0,
         fontWeight: FontWeight.w500,
         color: AppColors.text,
+        fontFamily: maven,
       ),
       bodyLarge: TextStyle(
         fontSize: 18.0,
         fontWeight: FontWeight.w400,
         color: AppColors.text,
+        fontFamily: maven,
       ),
       titleLarge: TextStyle(
         fontSize: 20.0,
         fontWeight: FontWeight.w400,
         color: AppColors.text,
+        fontFamily: maven,
       ),
       titleMedium: TextStyle(
         fontSize: 16.0,
         fontWeight: FontWeight.w500,
         color: AppColors.text,
+        fontFamily: maven,
       ),
     ),
   );
